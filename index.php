b6ea4fd7a4968bd69438c9a767722f660824ff01
Edwin de Jonge <edwindjonge@gmail.com>
2012-01-27 09:07:36 +0100

    ccs to css

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <title>SMP - tables</title>
-    <link rel="stylesheet" href="ccs/smp.css" type="text/css"/>
+    <link rel="stylesheet" href="css/smp.css" type="text/css"/>
   </head>
 
   <body>
