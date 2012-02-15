b5d7a672040933e2e580f4a10b83086f7bb7bc60
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2012-02-15 17:20:03 +0100

    Added calculation of margins to bedrijvendynamiek

--- R/bedrijvendynamiek/read_table.R
+++ R/bedrijvendynamiek/read_table.R
@@ -22,9 +22,39 @@ read_table <- function() {
                     labels=c("opvoering", "afvoering"))
     levels(data$grootteklasse) <- c("microbedrijf", "kleinbedrijf", "middenbedrijf",
                     "grootbedrijf")
-
     library(reshape2)
     data <- melt(data, id.var=c("jaar", "grootteklasse", "sbi", "effect"))
+
+    # calculate all margins
+    library(plyr)
+    data$value[data$effect == "afvoering"] <- - data$value[data$effect == "afvoering"]
+    m <- names(data)[names(data) != "value"]
+   
+    margins <- data.frame()
+    for (i in (seq_len(length(m))-1)) {
+      c <- combn(m, i)
+      for (j in seq_len(ncol(c))) {
+          margin <- ddply(data, c[,j], function(d) {
+              result <- d[1,]
+              result[, !(m %in% c[,j])] <- NA
+              result$value <- sum(d$value)
+              return(result)
+          })
+          margin$.id <- NULL
+          margins <- rbind(margins, margin)
+      }
+    }
+
+    # Change NA's in margins to TOTAL
+    for (i in seq_along(margins)) {
+        sel <- is.na(margins[, i])
+        margins[sel, i] <- "TOTAL"
+    }
+
+    # Add margins to data
+    data <- rbind(data, margins)
+    return(data)
+    
 }
 
 
