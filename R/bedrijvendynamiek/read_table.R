59dcc1ffdc2e4a169b2b47cdbf68dd39a2e9340e
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2012-03-02 16:28:41 +0100

    Added table with number of companies; function to add to database not complete

--- R/bedrijvendynamiek/read_table.R
+++ R/bedrijvendynamiek/read_table.R
@@ -9,6 +9,70 @@ table_info <- function(id) {
     )
 }
 
+read_aantallen <- function() {
+  data <- read.csv2("bedrijvendynamiek/aantallen.csv", header=FALSE)
+
+  data <- data[, c(-2, -5)]
+
+  names(data) <- c("sbi_naam", "gk", "jaar", "aantal")
+
+  data$sbi <- substr(data$sbi_naam, 1, 1)
+
+  data$grootteklasse <- NA
+  data$grootteklasse[data$gk == "1 werkzame persoon"] <- 1
+  data$grootteklasse[data$gk == "2 werkzame personen"] <- 2
+  data$grootteklasse[data$gk == "3 tot 5 werkzame personen"] <- 2
+  data$grootteklasse[data$gk == "5 tot 10 werkzame personen"] <- 2
+  data$grootteklasse[data$gk == "10 tot 20 werkzame personen"] <- 3
+  data$grootteklasse[data$gk == "20 tot 50 werkzame personen"] <- 3
+  data$grootteklasse[data$gk == "50 tot 100 werkzame personen"] <- 3
+  data$grootteklasse[data$gk == "100 werkzame personen of meer"] <- 4
+  data$grootteklasse <- factor(data$grootteklasse, levels=1:4, 
+     labels = c("microbedrijf", "kleinbedrijf", "middenbedrijf", "grootbedrijf"))
+
+  data$gk <- NULL
+  data$sbi_naam <- NULL
+
+  # calculate all margins
+  library(plyr)
+  m <- names(data)[names(data) != "aantal"]
+ 
+  margins <- data.frame()
+  for (i in (seq_len(length(m))-1)) {
+    c <- combn(m, i)
+    for (j in seq_len(ncol(c))) {
+        margin <- ddply(data, c[,j], function(d) {
+            result <- d[1,]
+            result[, m[!(m %in% c[,j])]] <- NA
+            result$aantal <- sum(d$aantal)
+            return(result)
+        })
+        margin$.id <- NULL
+        cat("calculating margin: ", paste(c[,j], collapse=", "), "\n")
+        print(margin)
+        margins <- rbind(margins, margin)
+    }
+  }
+
+  # calculate margins
+  library(plyr)
+  m1 <- ddply(data, "grootteklasse", function(d) {
+        r <- d[1,]
+        r$aantal <- sum(d$aantal)
+        return(r)
+      })
+  m1$sbi <- "TOTAL"
+  m2 <- ddply(data, "sbi", function(d) {
+        r <- d[1,]
+        r$aantal <- sum(d$aantal)
+        return(r)
+      })
+  m2$grootteklasse <- "TOTAL"
+
+  return(data)
+}
+
+
 read_table <- function() {
     data <- read.csv2("bedrijvendynamiek/dynamiek.csv")
 
