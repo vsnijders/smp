5ecebc15bee6cc83245b1eaa12cdba7161714297
Edwin de Jonge <edwindjonge@gmail.com>
2012-03-16 14:07:29 +0100

    sm working, but not plugged in smp

--- js/cross.js
+++ js/cross.js
@@ -1,91 +1,10 @@
-function Indexer(prop){
-   
-   var Indexer = {};
-   
-   var size = 0;
-   var map = {};
-
-   Indexer.indexer = function(o){
-      return map[o[prop]] = map[o[prop]] || size++
-   }
-   
-   Indexer.index = function(data){
-      return data.map(Indexer.indexer);
-   }
-      
-   Indexer.map = function(o){
-      if (!arguments.length) 
-         return map;
-      
-      return map[o[prop]];
-   }
-   
-   Indexer.size = function() {
-      return size;
-   }
-   
-   return Indexer;
-}
-
-function MultiIndexer(props){
-   
-   var MultiIndexer = {};
-   
-   var props = props;
-   var maps = props.map(function(p){return {}});
-   var sizes = props.map(function(p) {return 0});
-   
-   MultiIndexer.indexer = function(o){
-      var idx = [];
-      for (var i=0; i < props.length; i++){
-         return maps[i][o[props[i]]] = maps[i][o[props[i]]] || sizes[i]++      
-      }
-      return idx;
-   }
-   
-   MultiIndexer.index = function(data){
-      return data.map(MultiIndexer.indexer);
-   }
-      
-   MultiIndexer.map = function(){
-      return map;
-   }
-   
-   MultiIndexer.size = function() {
-      return size;
-   }
-   return MultiIndexer;
-}
-
-function cross2(data, row, column, callback){
-   row = row || [];
-   column = column || [];
-   
-   var vars = row.concat(column);
-   var idx = vars.map(Indexer);
-   idx.map(function(i){i.index(data)});
-   
-   var keys = idx.map(function(i){return i.map});
-   var sizes = idx.map(function(i){return i.size()});
-
-   console.log("keys", keys);
-   var nest = d3.nest();
-   for (var k in keys){
-      nest.key(keys[k]);
-   }
-
-   var d = nest.map(data);
-   console.log(idx.map(function(i) { return new Array(i.size())}))
-   return d;   
-}
-
 function cross(data, row, col, callback){
    row = row || [];
    col = col || [];
    
    var vars = row.concat(col);
    if (vars.length == 0)
-      return [[data]];
+      return {row: "", col: "", data: [[data]]};
 	  
    var rowcat = row.map(function(v){return function(d){return d[v]}});
    var colcat = col.map(function(v){return function(d){return d[v]}});
@@ -101,20 +20,41 @@ function cross(data, row, col, callback){
    
    var d = rows.map(function(r){
       return cols.map( function(c){
-	     return cross[r][c] || [];
+	     var rcdata = cross[r][c] || [];
+		 return rcdata;
 	  });
    });
    //console.log("cross", cross);
    //console.log("d", d);
-   return d;   
+   return { row: rows
+          , col: cols
+		  , data: d
+          };   
 }
 
-var data = [ {a: 1, b: 1}
-           , {a: 1, b: 2}
-           , {a: 4, b: 1}
+function smallmul(chart, crosseddata, charttype){
+  var row = crosseddata.row;
+  var col = crosseddata.col;
+  var data = crosseddata.data;
+  
+  var table = chart.append("table");
+  table.selectAll("tr").data(data)
+     .enter()
+	 .append("tr")
+	 .selectAll("td").data(data)
+	   .enter()
+	   .append("td")
+	   .append("svg")
+	   .text(BarChart(this, data));
+}
+
+var data = [ {a: "a1", b: "b1"}
+           , {a: "a1", b: "b2"}
+           , {a: "a4", b: "b1"}
            ];
 
 var vars = ["a", "b"];
 
 var d = cross(data, ["a"], ["b"]);
+smallmul(d3.select('.graph'), d, "line");
 console.log(d);
