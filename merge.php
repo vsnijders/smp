88ae9f06b26fedf8ed099068593b3cd129988334
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2012-05-04 16:03:41 +0200

    Added empty page for merging tables

---
+++ merge.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+
+    <title>StatMine</title>
+
+    <!-- webpage icon -->
+    <link type="text/css" href="css/ui.css" rel="stylesheet" />
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/vnd.microsoft.icon" />
+    <link rel="icon" href="img/favicon2.png" type="image/png" />
+
+    <!-- jQuery includes -->
+    <link type="text/css" href="css/smoothness/jquery-ui-1.8.17.custom.css" rel="stylesheet" />
+    <script type="text/javascript" src="js/jquery-1.7.1.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.8.17.custom.min.js"></script>
+
+  </head>
+  <body>
+   
+  <header>
+    <h1><a href="index.php">StatMine</a></h1>
+  </header>
+  <div id="navigation-container">
+    <div id="navigation">
+      <ul>
+        <li><a href="index.php">Tables</a></li>
+        <li><a href="help/index.php">Help</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <article>
+    <div class="menu">
+    </div>
+
+    <div class="content">
+
+     Here be monsters.
+
+    </div>
+  </article>
+
+  <footer>
+  </footer>
+
+  </body>
+</html>
+
+
