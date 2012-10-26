edb040c7039b71595100aa89f235a4c6121aad7e
Edwin de Jonge <edwindjonge@gmail.com>
2012-10-26 16:08:43 +0200

    added transport

--- R/util.R
+++ R/util.R
@@ -21,37 +21,30 @@ cleanUpSL <- function(file, skip = 1, colnames=NULL){
   file2 <- file.path(dirname(file), "data.csv")
   write.csv(tab, file2, row.names=FALSE)
   
-  writeMeta(tab, dirname=dirname(file2))
+  #writeMeta(tab, dirname=dirname(file2))
   
   invisible(tab)
 }
 
 writeMeta <- function(dat, dirname){
   file <- file.path(dirname, "data.yml")
+  
+  
   vars <- sapply(dat, function(x){
     if (is.factor(x)){
-      levels(x)
+      list( name=NULL
+          , levels=levels(x)
+          , labels=levels(x)
+          )
     } else {
-      storage.mode(x)
+      list( name=NULL
+          , type=storage.mode(x)
+          )
     }
   })
+  
+  writeLines(file, as.yaml(vars))
 }
 
-cleanUpSL("bedrijvenfaillisementen/Uitgesproken_faillis_raw.csv")
-
-
-file <- "bedrijvenfaillisementen/Uitgesproken_faillis_raw.csv"
-
-f <- readLines(file)
-# remove last line
-f <- head(f, -1)
-
-# change decimal sign
-f <- gsub("(\\d)\\.(\\d)", "\\1,\\2", f)
-
-writeLines(f, "test.csv")
-skip <- 1
-tab <- read.csv2("test.csv", header=TRUE, skip=skip, na.strings=c("-",".","x"))
-str(tab)
-
-head(f)
+tab <- cleanUpSL("bedrijvenfaillisementen/Uitgesproken_faillis_raw.csv")
+tab <- cleanUpSL("transport/Transportbedrijven_raw.csv", skip=3)
