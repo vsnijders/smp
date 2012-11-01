efd5b475f9ccaeee50a184c702942eeb74590ddf
Jan van der Laan <djvanderlaan@unrealizedtime.nl>
2012-11-01 17:25:08 +0100

    Added error handling to app_get_meta

--- rook/R/app_get_meta.R
+++ rook/R/app_get_meta.R
@@ -3,9 +3,18 @@ app_get_meta <- function(env){
   req <- Rook::Request$new(env)
   res$header("Content-Type", "application/json")
   table_name <- req$params()$table
-  table <- get_table(table_name, data=FALSE)
-  # TODO error handling if table does not exist
-  res$write(toJSON(table$meta))
+  if (is.null(table_name)) {
+    res$write(toJSON(list(fail=TRUE, message="table not given.")))
+  } else {
+    tryCatch({
+      table <- get_table(table_name, data=FALSE)
+      res$write(toJSON(table$meta))
+    }, error = function(e) {
+      res$write(toJSON(list(fail=TRUE, 
+        message=paste0("The table '", table_name, "' could not be opened."))))
+      
+    })
+  }
   res$finish()
 }
 
