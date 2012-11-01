6a32f6892bd91e0d0dc046ce33872be1b9ce2e27
Jan van der Laan <djvanderlaan@unrealizedtime.nl>
2012-11-01 17:06:31 +0100

    Modified list_tables: empty directories are now accepted

--- rook/R/list_tables.R
+++ rook/R/list_tables.R
@@ -1,11 +1,13 @@
 list_tables <- function(table_dirs = c("data/tmp", "data/tables")) {
   tables <- list()
   for (dir in table_dirs) {
-      tlist  <- fromJSON(file=paste0(dir, "/tables.json"))
-      for (table in names(tlist)) {
-        tables[[table]]     <- tlist[[table]]
-        tables[[table]]$dir <- dir
-      }
+      try({
+        tlist  <- fromJSON(file=paste0(dir, "/tables.json"))
+        for (table in names(tlist)) {
+          tables[[table]]     <- tlist[[table]]
+          tables[[table]]$dir <- dir
+        }
+      }, silent = TRUE)
   }
   return(tables)
 }
