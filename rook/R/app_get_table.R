9c5956616723fa5a0eb1f83525092567c0f77075
Jan van der Laan <djvanderlaan@unrealizedtime.nl>
2012-11-01 17:21:12 +0100

    Added error handling to app_get_table and table.html

--- rook/R/app_get_table.R
+++ rook/R/app_get_table.R
@@ -3,9 +3,18 @@ app_get_table <- function(env){
   req <- Rook::Request$new(env)
   res$header("Content-Type", "application/json")
   table_name <- req$params()$table
-  table <- get_table(table_name, meta=FALSE)
-  # TODO error handling if table does not exist
-  res$write(df_to_json(table$data))
+  if (is.null(table_name)) {
+    res$write(toJSON(list(fail=TRUE, message="table not given.")))
+  } else {
+    tryCatch({
+      table <- get_table(table_name, meta=FALSE)
+      res$write(df_to_json(table$data))
+    }, error = function(e) {
+      res$write(toJSON(list(fail=TRUE, 
+        message=paste0("The table '", table_name, "' could not be opened."))))
+      
+    })
+  }
   res$finish()
 }
 
