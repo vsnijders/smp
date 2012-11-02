0282c1ec9fe1d761dc1aeb1adcb3a2337bcba23a
Edwin de Jonge <edwindjonge@gmail.com>
2012-11-02 16:02:30 +0100

    batman test

---
+++ rook/R/app_tables.R
@@ -0,0 +1,17 @@
+#' Rook application that returns the data of a table in JSON format
+#'
+#' @param env Rook environment variable
+#'
+#' @return Rook Request object
+#'
+#' @export
+#'
+app_tables <- function(env){
+  res <- Rook::Response$new()
+  res$header("Content-Type", "application/json")
+
+  req <- Rook::Request$new(env)
+  tables <- data.frame(name=names(list_tables()))
+  res$write(df_to_json(tables))
+  res$finish()
+}
