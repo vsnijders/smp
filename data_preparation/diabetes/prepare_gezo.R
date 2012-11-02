49dbf289ba63c7bda80e2d5752f46e7fd67cd039
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2012-11-02 15:58:59 +0100

    Added extra meta data to tables

--- data_preparation/diabetes/prepare_gezo.R
+++ data_preparation/diabetes/prepare_gezo.R
@@ -1,6 +1,7 @@
 
 source("../settings.R", chdir=TRUE)
 library(rjson)
+library(yaml)
 
 # read data
 data <- read.csv2("gezo.csv", dec='.', na.strings='.')
@@ -47,7 +48,16 @@ o <- order(data$jaar, data$leeftijd, data$geslacht)
 data <- data[o, ]
 
 # Create meta
-meta <- create_meta(data)
+meta_file <- "gezo.yaml"
+if (!file.exists(meta_file)) {
+  meta <- create_meta(data)
+  write(as.yaml(meta), file=meta_file)
+  cat(paste("No file '", meta_file, "' containing the meta data for the file ",
+    "was found. A default meta data file is generated. Edit this file and run ", 
+    "this script again to regenerate the meta data for this table. For now ",
+    "the default meta data is used.\n", sep=""))
+} 
+meta <- yaml.load_file(meta_file)
 write(toJSON(meta), file=paste0(TABLE_DIR, "/gezo_meta.json"))
 
 # Save data
--- data_preparation/diabetes/prepare_huisartspatienten.R
+++ data_preparation/diabetes/prepare_huisartspatienten.R
@@ -1,6 +1,7 @@
 
 source("../settings.R", chdir=TRUE)
 library(rjson)
+library(yaml)
 
 # read data
 data <- read.csv2("huisartspatienten.csv", dec='.', na.strings='.')
@@ -58,11 +59,19 @@ o <- order(data$jaar, data$leeftijd, data$geslacht)
 data <- data[o, ]
 
 # Create meta
-meta <- create_meta(data)
-library(rjson)
+meta_file <- "huisartspatienten.yaml"
+if (!file.exists(meta_file)) {
+  meta <- create_meta(data)
+  write(as.yaml(meta), file=meta_file)
+  cat(paste("No file '", meta_file, "' containing the meta data for the file",
+    "was found. A default meta data file is generated. Edit this file and run", 
+    "this script again to regenerate the meta data for this table. For now the",
+    "default meta data is used.\n", sep=""))
+} 
+meta <- yaml.load_file(meta_file)
 write(toJSON(meta), file=paste0(TABLE_DIR, "/huisartspatienten_meta.json"))
 
-
+# Save data
 write.table(data, paste0(TABLE_DIR, "/huisartspatienten.csv"), sep=",", 
   quote=FALSE, na="", row.names=FALSE)
 
