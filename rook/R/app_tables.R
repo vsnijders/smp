0650d9049117c82fd2e3201467146a2a7497966f
Edwin de Jonge <edwindjonge@gmail.com>
2012-11-07 23:04:39 +0100

    added data

--- rook/R/app_tables.R
+++ rook/R/app_tables.R
@@ -11,12 +11,8 @@ app_tables <- function(env){
   res$header("Content-Type", "application/json")
 
   req <- Rook::Request$new(env)
-<<<<<<< HEAD
   tables <- as.data.frame(do.call(rbind,list_tables()))
   print(tables)
-=======
-  tables <- data.frame(name=names(list_tables()))
->>>>>>> a02b89f6ef420dd0db4eb96c9c5cb3dce307f8df
   res$write(df_to_json(tables))
   res$finish()
 }
