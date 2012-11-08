9b9e3e1152aafa5d9261544285fdc3edc551c6f7
Edwin de Jonge <edwindjonge@gmail.com>
2012-11-08 10:48:56 +0100

    tweaking

--- data_preparation/settings.R
+++ data_preparation/settings.R
@@ -1,3 +1,4 @@
+library(yaml)
 
 # Define some paths
 setwd("../")
@@ -12,16 +13,15 @@ rfiles <- list.files(R_DIR, pattern="\\.[rR]$")
 for (file in rfiles) source(paste0(R_DIR, "/", file))
 
 # load meta file or create if not available
-load_meta <- function(data, meta_file="data.yaml"){
+load_meta <- function(data, meta_file="data.yaml", ...){
   if (!file.exists(meta_file)) {
-    meta <- create_meta(data)
+    meta <- create_meta(data, ...)
     write(as.yaml(meta), file=meta_file)
     cat(paste("No file '", meta_file, "' containing the meta data for the file ",
               "was found. A default meta data file is generated. Edit this file and run ", 
               "this script again to regenerate the meta data for this table. For now ",
               "the default meta data is used.\n", sep=""))
   }
-  stopifnot(require(yaml))
   meta <- yaml.load_file(meta_file)
   meta
 }
--- data_preparation/veiligheid/prepare_crimi.R
+++ data_preparation/veiligheid/prepare_crimi.R
@@ -1,11 +1,13 @@
-library(rjson)
+source("../settings.R", chdir=TRUE)
 
-oldwd <- setwd("data_preparation/")
-source("SL_cleanup.R")
-source("settings.R")
+# read data
+data <- read.csv2("crimi.csv", dec='.', na.strings=NA_STRINGS)
 
-dat <- SLcleaner("data_preparation/veiligheid/Geregistreerde_crimi.csv")
+names(data) <- c("Regio", "Soort", "Jaar", "Aantal misdrijven", 
+                 "Misdrijven per 1000 inwoners", "Opgehelderde misdrijven", 
+                 "Perc. opgehelderd")
+# Create meta
+meta <- load_meta(data, "crimi.yaml", pop_name="Geregistreerde misdrijven")
 
-add_table("crimi", data=dat$data, meta=dat$meta)
-
-setwd(oldwd)
+# Save data
+add_table("crimi", data, meta, TABLE_DIR)
