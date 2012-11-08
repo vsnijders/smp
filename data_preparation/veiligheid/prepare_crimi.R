69962f8b76dc0177f9a53c057c73431e0dd32cfc
Edwin de Jonge <edwindjonge@gmail.com>
2012-11-08 13:29:24 +0100

    crimi (again...)

--- data_preparation/veiligheid/prepare_crimi.R
+++ data_preparation/veiligheid/prepare_crimi.R
@@ -3,9 +3,11 @@ source("../settings.R", chdir=TRUE)
 # read data
 data <- read.csv2("crimi.csv", dec='.', na.strings=NA_STRINGS)
 
-names(data) <- c("Regio", "Soort", "Jaar", "Aantal misdrijven", 
+names(data) <- c("Regio", "Misdrijf", "Jaar", "Aantal misdrijven", 
                  "Misdrijven per 1000 inwoners", "Opgehelderde misdrijven", 
                  "Perc. opgehelderd")
+levels(data$Misdrijf) <- c("Vermogensmisdrijven", "Vernielingen/misdr.openb.orde", 
+                           "Gewelds- en seksuele misdrijven", "Totaal")
 # Create meta
 meta <- load_meta(data, "crimi.yaml", pop_name="Geregistreerde misdrijven")
 
