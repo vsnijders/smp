f9b7866760020c7bebf5902b37ea376fd07b12bd
Edwin de Jonge <edwindjonge@gmail.com>
2012-11-09 10:10:11 +0100

    Updated description

--- data_preparation/prepare_all.R
+++ data_preparation/prepare_all.R
@@ -1,5 +1,9 @@
 
 source("diabetes/prepare_gezo.R", chdir=TRUE)
 source("diabetes/prepare_huisartspatienten.R", chdir=TRUE)
+
+source("veiligheid/prepare_crimi.R", chdir=TRUE)
 source("veiligheid/prepare_slachtoffer.R", chdir=TRUE)
 
+source("energie/prepare_energiebalans.R", chdir=TRUE)
+source("energie/prepare_productie.R", chdir=TRUE)
