c83af0be7d554209663b771126924899282a6fed
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2012-11-09 13:44:32 +0100

    Finished work on variable selection and filtering in visualise.html

---
+++ rook/www/js/visualise_menu.js
@@ -0,0 +1,126 @@
+
+// =================== GLOBAL VARIABLES ========================
+var selection = {};
+var filter = {};
+var graphtype;
+
+function update_filter() {
+  var context = $(this);
+  if (!context.is('input')) context = $('#' + graphtype);
+  filter = {};
+  $("input.filter:checked", context.closest(".tab-pane")).each(function() {
+    var variable = this.name;
+    var level    = this.value;
+    if (filter[variable] === undefined) filter[variable] = [];
+    filter[variable].push(level);
+  });
+}
+
+function update_selection(context) {
+  if (context === undefined) context = $('#' + graphtype);
+  selection = {};
+  $("div.plotvariable .droppable", $(context).closest(".tab-pane")).each(function(i, ul) {
+    var dimension = $(ul).attr("data-dimension");
+    selection[dimension] = [];
+    $("li.draggable", $(ul)).each(function(j, li) {
+      selection[dimension][j] = $(li).attr("data-variable");
+    });
+  })
+}
+
+
+$(function() {
+
+
+  var tablename = $(document).getUrlParam("table");
+  jQuery.getJSON("r/get_meta.r", {"table":tablename}, 
+      function(data) {
+    if (data.fail) {
+      alert("Error fetching data");
+      return;
+    }
+
+    // TODO: following code block needs cleanup
+    $(".variables").each(function(i, el) {
+
+      // add dimensions to page
+      jQuery.each(data.dimensions, function(dim, dat) {
+        var li = $("<li>").addClass("draggable categorical")
+          .attr("data-variable", dim).text(dat.name)
+          .draggable({
+            revert : "invalid",
+            axis : "y"
+          });
+        var a = $("<a>").attr("href", "#").addClass("togglefilter")
+          .html('<i class=\"icon-filter"></i>').appendTo(li)
+          .click(function() {
+            $(this).next().toggle('slow');
+            return false;
+          });
+          
+        var div = $("<div>").addClass("filter").appendTo(li).hide();
+        var form = $("<form>").appendTo(div);
+        jQuery.each(dat.levels, function(i, lab) {
+          var label = $("<label>").text(lab).appendTo(form);
+          $("<input>").attr("type", "checkbox").addClass("filter")
+            .attr("name", dat.name).val(lab).click(update_filter)
+            .prependTo(label);
+        });
+        $(el).append(li)
+      });
+
+
+      // add variables to page
+      jQuery.each(data.variables, function(dim, dat) {
+        var li = $("<li>").addClass("draggable numeric")
+          .attr("data-variable", dim).text(dat.name)
+          .draggable({
+            revert : "invalid",
+            axis : "y"
+          });
+        $(el).append(li)
+      });
+
+    });
+
+  });
+
+
+  // ================== VARIABLE SELECTION =======================
+  // Create tabbed pages for each of the graph types
+  $("#tabs").tabs();
+  // Keep track of which tab = graphtype is selected
+  $('a[data-toggle="tab"]').on('shown', function (e) {
+    graphtype = $(this).text();
+    update_selection();
+    update_filter();
+  });
+  // Select first tab
+  $('.nav a:first').tab("show");
+
+  // Drag-and-drop of variables on graph dimensions
+  $(".droppable").droppable({
+    accept: ".draggable",
+    activeClass: "droppable_active", 
+    hoverClass: "droppable_hover", 
+    tolerance : "touch",
+    drop: function(event, ui) {
+      var granpa = $(this).parent().parent();
+      // move existing variables to variables section
+      $(".draggable", $(this)).appendTo($(".variables", granpa));
+      // append newly dropped variable to the list
+      $(ui.draggable).prependTo($(this)).attr("style", "position:relative");
+      // update selection
+      update_selection(this);
+    },
+    accept : function(draggable) {
+      return (($(this).hasClass("numeric") && $(draggable).hasClass("numeric")) || 
+        ($(this).hasClass("categorical") && $(draggable).hasClass("categorical")) ||
+        $(this).hasClass("variables"))
+    }
+
+
+  });
+
+});
+
