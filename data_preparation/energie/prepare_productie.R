7cd5d76cf7047646fe7e61f1516e0c247093bf79
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2012-11-14 11:56:07 +0100

    Corrected errors/problems in data and meta of table productie

--- data_preparation/energie/prepare_productie.R
+++ data_preparation/energie/prepare_productie.R
@@ -1,13 +1,16 @@
 source("../settings.R", chdir=TRUE)
 
 # read data
-data <- read.csv2("productie.csv", dec='.', na.strings=NA_STRINGS)
+data <- read.csv2("productie.csv", dec='.', na.strings=NA_STRINGS, fileEncoding="latin1")
 data <- data[c(1,2,4,12)]
-names(data) <- c("Sector", "Jaar", "Bruto toegevoegde waarde basisprijzen (2005)", "Bruto.toegevoegde.waarde.basisprijzen (index)")
+names(data) <- c("Sector", "Jaar", "BrutoToegevoegdeWaardeBasisprijzen", "BrutoToegevoegdeWaardeBasisprijzenIndex")
 
 levels(data$Sector) <- gsub(",", " ", levels(data$Sector))
 # remove "," 
 levels(data$Sector) <- gsub(",", " ", levels(data$Sector))
+
+# Remove the * from Jaar
+levels(data$Jaar) <- gsub("\\**$", "", levels(data$Jaar))
 
 # Create meta
 meta <- load_meta(data, "productie.yaml", name="Toegevoegde waarde per bedrijfstak", description="Bbp, productie en bestedingen; productie en inkomens naar bedrijfstak", pop_name="Bedrijven")
