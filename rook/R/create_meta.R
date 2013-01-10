e8b862a9160b7020c3f45de89d9128453fa55a3e
Edwin de Jonge <edwindjonge@gmail.com>
2013-01-10 13:51:35 +0100

    implemented create_meta complexer levels

--- rook/R/create_meta.R
+++ rook/R/create_meta.R
@@ -8,7 +8,8 @@ create_meta <- function(data, name = "Name missing", description = "",
         variables = list())
     for (dim in dimensions) {
         lvls <- levels(data[[dim]])
-        d <- list( name = dim, description="", levels = lvls,
+        cats <- lapply(lvls, function(l) {list(level=l, name=l, description="")})
+        d <- list( name = dim, description="", categories = cats,
                    aggregate = lvls[length(lvls)], default = lvls[length(lvls)],
                    type = "categorical"
                  )
@@ -29,3 +30,22 @@ create_meta <- function(data, name = "Name missing", description = "",
     return(meta)
 }
 
+
+### testing
+#create_meta(iris)
+
+# convertmeta <- function(meta){
+#   dimensions <- lapply(meta$dimensions, function(d){
+#     d$categories <- lapply(d$levels, function(l){ list(level=l, name=l, description="")})
+#   })
+#   meta$dimensions <- dimensions
+#   meta
+# }
+# 
+# require(yaml)
+# for (m in dir(".", "*.yaml", recursive=T)[1]){
+#   print(m)
+#   meta <- yaml.load_file(m)
+#   meta <- convertmeta(meta)
+#   #write(as.yaml(meta), m)
+# }
